e51c3447cf6f26690e1d54abff8a8c2e47d3bd18
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-06-18 21:57:38 -0300

    Update 02_Source.sh

--- 02_Source.sh
+++ 02_Source.sh
@@ -1,1 +1,3 @@
-wget -c https://github.com/Kitware/CMake/releases/download/v${CMAKE}/cmake-${CMAKE}.tar.gz
+url="https://github.com/Kitware/CMake/releases/latest/" ; \
+CMAKE=$(curl -sSL $url | grep -o 'tag/v[0-9.]*' | head -n 1 | cut -d '/' -f 2 | sed 's/^v//') ; \
+wget -c "https://github.com/Kitware/CMake/releases/download/v${CMAKE}/cmake-${CMAKE}.tar.gz"
